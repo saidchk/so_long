b8c5f2e9c4ac8cb8b161d4946c8914ede30c1ecc
Said Chakkour <schakkou@c2r5p3.1337.ma>
2024-04-28 21:53:44 +0100

    v,f

--- 1so_long/move_ghost.c
+++ 1so_long/move_ghost.c
@@ -99,26 +99,39 @@ void  move(s_data *game)
 
 	//printf("P1-----(%i, %i)=-----\n",game->x, game->y);
    //printf("B1-----(%i, %i)=-----\n",game->ennemi.x_g, game->ennemi.y_g);
-    if (game->ennemi.y_g > game->y && game->map[game->ennemi.y_g - 1][game->ennemi.x_g] != '1')
+   
+	if (game->ennemi.y_g == game->y || game->ennemi.x_g == game->x && )
+	{
+		if (game->map[game->ennemi.y_g][game->ennemi.x_g + 1] != '1')
+		 	move_right_ennemi(game);
+		else if (game->map[game->ennemi.y_g][game->ennemi.x_g - 1] != '1')
+			move_left_ennemi(game);
+		else if ( game->map[game->ennemi.y_g - 1][game->ennemi.x_g] != '1')
+			move_up_ennemi(game);
+		else
+			move_down_ennemi(game);
+	}
+     else if (game->ennemi.y_g > game->y && game->map[game->ennemi.y_g - 1][game->ennemi.x_g] != '1')
         move_up_ennemi(game);
     else if (game->ennemi.y_g < game->y && game->map[game->ennemi.y_g + 1][game->ennemi.x_g] != '1')
         move_down_ennemi(game);
+
 	//printf("A1-----(%i, %i)=-----\n",game->ennemi.x_g, game->ennemi.y_g);
     if (game->ennemi.x_g > game->x && game->map[game->ennemi.y_g][game->ennemi.x_g - 1] != '1')
         move_left_ennemi(game);
     else if (game->ennemi.x_g < game->x && game->map[game->ennemi.y_g][game->ennemi.x_g + 1] != '1')
        move_right_ennemi(game);
-    else if (game->check)
-    {
-        if (game->ennemi.x_g > game->x && game->ennemi.y_g > game->y && game->map[game->ennemi.y_g - 1][game->ennemi.x_g] != '1' )
-            move_up_ennemi(game);
-        else if ( game->map[game->ennemi.y_g + 1][game->ennemi.x_g] != '1')
-        	move_down_ennemi(game);
-		else if ( game->map[game->ennemi.y_g][game->ennemi.x_g - 1] != '1')
-        move_left_ennemi(game);
-    	else if ( game->map[game->ennemi.y_g][game->ennemi.x_g + 1] != '1')
-       	move_right_ennemi(game);
-    }
+    // else if (game->check)
+    // {
+    //     if (game->ennemi.x_g > game->x && game->ennemi.y_g > game->y && game->map[game->ennemi.y_g - 1][game->ennemi.x_g] != '1' )
+    //         move_up_ennemi(game);
+    //     else if ( game->map[game->ennemi.y_g + 1][game->ennemi.x_g] != '1')
+    //     	move_down_ennemi(game);
+	// 	else if ( game->map[game->ennemi.y_g][game->ennemi.x_g - 1] != '1')
+    //     move_left_ennemi(game);
+    // 	else if ( game->map[game->ennemi.y_g][game->ennemi.x_g + 1] != '1')
+    //    	move_right_ennemi(game);
+    // }
 	//printf("A2-----(%i, %i)=-----\n",game->ennemi.x_g, game->ennemi.y_g);
 
 }
